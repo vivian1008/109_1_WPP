55409cf554a69d37b4cd5293709b2c627f9dc423
vivian1008 <56061904+vivian1008@users.noreply.github.com>
2021-01-14 15:33:16 +0800

    Add files via upload

--- MainFrame.java
+++ MainFrame.java
@@ -2,11 +2,14 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.lang.*;
+import java.util.Random;
 
 public class MainFrame extends JFrame{
+    private Random rd;
+    private boolean flag=true;
     private int x,y,u,v;
     private int min,scd,msd = 0;
-    private Timer tm;
+    private Timer tm,tm2;
     private JButton jgo = new JButton("Go!");
     private JButton jet = new JButton("Exit");
     private JButton jan = new JButton("Again");
@@ -21,9 +24,13 @@ public MainFrame(){
     init();
 }
 private void init(){
+    rd=new Random();
+    x=rd.nextInt(500);
+    y=rd.nextInt(500);
     this.setLayout(null);
     this.setLocation(100,50);
-    this.setSize(500,400);
+    this.setSize(x = 500,y = 400);
+   
 
     for(int i = 0; i < 5; i++) {
         String s = "" + i;
@@ -42,8 +49,8 @@ private void init(){
     jdn.setBounds(375,275,50,25);
     jlt.setBounds(425,300,50,25);
     jrt.setBounds(325,300,50,25);
-    jlb.setBounds(u=250,v=150,50,50);
-    jdt.setBounds(x = 300,y = 100,50,50);
+    jlb.setBounds(u = 250,v = 150,50,50);
+    jdt.setBounds(x = 300, y = 200,50,50);
     tim[0].setBounds(50,20,10,10);
     tim[1].setBounds(70,20,10,10);
     tim[2].setBounds(90,20,10,10);
@@ -58,15 +65,21 @@ private void init(){
     this.add(jlt);
     this.add(jrt);
     this.add(jlb);
-    this.add(jdt);
     this.add(tim[0]);
     this.add(tim[1]);
     this.add(tim[2]);
     this.add(tim[3]);
-    //this.add(tim[4]);
+    this.add(jdt);
+
+    if(rd.nextInt(10)%2==0){
+        flag=true;
+    }else {
+        flag=false;
+    }
+
 
     tm = new Timer(100, new ActionListener() {
-       public void actionPerformed(ActionEvent e) {
+       public void actionPerformed(ActionEvent e) {        
            msd++;
            if(msd == 10) {
                msd = 0;
@@ -95,6 +108,28 @@ private void init(){
     jgo.addActionListener(new ActionListener() {
            public void actionPerformed(ActionEvent ae) {
            tm.start();
+           tm2 = new Timer(rd.nextInt(500), new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if(flag) {
+                    if(x-10>0){
+                        jdt.setLocation(x-=10,y);
+                    }else if(y-10>0){
+                        jdt.setLocation(x,y-=20);
+                    }else {
+                        flag=!flag;
+                    }
+                }else {
+                    if(x+10<400){
+                        jdt.setLocation(x+=10,y);
+                    }else if(y+10<500){
+                        jdt.setLocation(x,y+=20);
+                    }else {
+                        flag=!flag;
+                    }
+                }
+            }
+        });
+        tm2.start();
            }
        });
 
@@ -112,7 +147,7 @@ private void init(){
                 tim[0].setText(Integer.toString(min = 0));
                 tm.stop();
                 jlb.setLocation(u=250,v=150);
-                jdt.setLocation(x=300,y=100);
+                jdt.setLocation(300,200);
             }
         });
 
@@ -120,28 +155,25 @@ private void init(){
     jup.addActionListener(new ActionListener(){
         public void actionPerformed(ActionEvent ae){
             jlb.setLocation(u,v = v-10);
-            jdt.setLocation(x,y = y-10);
+
         }
     });
 
     jdn.addActionListener(new ActionListener(){
         public void actionPerformed(ActionEvent ae){
             jlb.setLocation(u,v = v + 10);
-            jdt.setLocation(x,y = y + 10);
         }
     });
 
     jlt.addActionListener(new ActionListener(){
         public void actionPerformed(ActionEvent ae){
             jlb.setLocation(u = u-10 ,v);
-            jdt.setLocation(x = x-10 ,y);
         }
     });
 
     jrt.addActionListener(new ActionListener(){
         public void actionPerformed(ActionEvent ae){
             jlb.setLocation(u = u+10 ,v);
-            jdt.setLocation(x = x+10 ,y);
         }
     });
 }
